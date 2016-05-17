b0e7c306f6f79b163795a02ff4a0d7ec2ea24948
Joshua <hiaggsmods@gmail.com>
2016-05-17 12:47:42 -0400

    Hopefully Fixed

--- src/main/java/com/hiagg/hiaggium/Hiaggium.java
+++ src/main/java/com/hiagg/hiaggium/Hiaggium.java
@@ -1,17 +1,6 @@
-/*
- * Decompiled with CFR 0_110.
- * 
- * Could not load the following classes:
- *  cpw.mods.fml.common.Mod
- *  cpw.mods.fml.common.Mod$EventHandler
- *  cpw.mods.fml.common.event.FMLInitializationEvent
- *  cpw.mods.fml.common.event.FMLPostInitializationEvent
- *  cpw.mods.fml.common.event.FMLPreInitializationEvent
- */
 package com.hiagg.hiaggium;
 
 import com.hiagg.blocks.ModBlocks;
-import com.hiagg.blocks.tile.ModTileEntities;
 import com.hiagg.creativetabs.ModTabs;
 import com.hiagg.hiaggium.CraftingManager;
 import com.hiagg.item.ModItems;
@@ -20,8 +9,6 @@ import com.hiagg.lib.RefStrings;
 
 import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.Mod;
-import cpw.mods.fml.common.Mod.EventHandler;
-import cpw.mods.fml.common.Mod.Instance;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
@@ -29,27 +16,20 @@ import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 @Mod(modid=RefStrings.MODID, name=RefStrings.NAME, version=RefStrings.VERSION)
 
 public class Hiaggium {
-	
-
-	@Instance("hiagg")
-	public static Hiaggium instance;
-	
-    @EventHandler
+    @Mod.EventHandler
     public static void PreLoad(FMLPreInitializationEvent event) {
         ModTabs.intialiseTabs();
         ModItems.mainRegistry();
         ModBlocks.mainRegistry();
-        ModTileEntities.registerTileEntities();
         CraftingManager.mainRegistry();
     }
 
-    @EventHandler
+    @Mod.EventHandler
     public static void Load(FMLInitializationEvent event) {
     	FMLCommonHandler.instance().bus().register(new KeyInputHandler());
     }
 
-    @EventHandler
+    @Mod.EventHandler
     public static void PostLoad(FMLPostInitializationEvent event) {
     }
 }
-
